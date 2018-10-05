c7b4b1f082c5166b521ac5fb753addf3c91d31a0
Conner Aiken <conner@fittedtech.com>
2018-10-05 13:43:42 -0700

    Add sequelize association example
    Add example of sequelize association http://docs.sequelizejs.com/class/lib/associations/base.js~Association.html

--- src/server/scripts/updateDb.js
+++ src/server/scripts/updateDb.js
@@ -7,6 +7,11 @@ let storesSql = fs.readFileSync(path.join(__dirname, 'stores.sql')).toString();
 let usersSql = fs.readFileSync(path.join(__dirname, 'users.sql')).toString();
 let userInfosSql = fs.readFileSync(path.join(__dirname, 'userInfos.sql')).toString();
 
+
+// Example of 1:1 Association
+// db.Sequelize.Order.belongsTo(User, {foreignKey: 'requestorId'});
+
+
 db.sequelize.sync({force: true})
 .then(response => db.sequelize.query(storesSql, { type: db.sequelize.QueryTypes.INSERT})) 
 .then(response => db.sequelize.query(usersSql, { type: db.sequelize.QueryTypes.INSERT})) 
